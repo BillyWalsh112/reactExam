6ececd38ef182c71fb32e6f83bd9fcecde08714a
BillyWalsh112 <20107248@mail.wit.ie>
2025-10-24 15:21:33 +0100

    Added cactch for the gettopratedmovie in api

--- src/api/tmdb-api.jsx
+++ src/api/tmdb-api.jsx
@@ -36,12 +36,18 @@ export const getMovie = (args) => {
 export const getTopRatedMovies = async () => {
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.VITE_TMDB_KEY}&language=en-US&page=1`
-  );
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return response.json();
-};
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
 
   export const getGenres = () => {
     return fetch(
